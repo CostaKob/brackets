e9dd2ed3341dd091b3ffb207508b17d94b4ce2f0
Costa <costakob@gmail.com>
2021-02-13 18:47:47 +0200

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = []
+  let letters = str.split("")
+  let brackets = ['(','{','[',']','}',')']
+
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      // open
+      if (letters[i] === bracketsConfig[j][0]) {
+        if(stack.includes(letters[i]) && !(brackets.includes(letters[i]))) {
+          console.log("1pop")
+          console.log(stack)
+          stack.pop()
+        }
+        else {
+          console.log("2push")
+          console.log(stack)
+          stack.push(letters[i])
+        }
+      }
+      // close
+      else if (letters[i] === bracketsConfig[j][1]) {
+        //if stack empty
+        if (stack.length !== 0 && stack[stack.length - 1] !== bracketsConfig[j][0]) {
+          return false
+        }
+        else {
+          console.log("3pop")
+          console.log(stack)
+          stack.pop()
+        }
+      }
+    }
+  }
+  return stack.length === 0 ? true : false
 }
